8a15673c3e81178de6c102d7fce0f83da5029573
shakkeera-jpg <shakkeerawindow@gmail.com>
2026-01-19 11:35:22 +0530

    Test sentry integration

--- E_commerce/urls.py
+++ E_commerce/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
+    path('sentry-debug/', trigger_error),
     path('admin/', admin.site.urls),
     path('api/',include('users.urls')),
     path('api/',include('products.urls')),
