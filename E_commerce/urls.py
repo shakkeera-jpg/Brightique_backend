5964ca407d20e69c62c157eb6c6fe95bc5b6f11e
shakkeera-jpg <shakkeerawindow@gmail.com>
2026-01-19 12:42:47 +0530

    chore: remove sentry debug path

--- E_commerce/urls.py
+++ E_commerce/urls.py
@@ -17,12 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sentry-debug/', trigger_error),
     path('api/',include('users.urls')),
     path('api/',include('products.urls')),
     path('api/',include('wishlist.urls')),
